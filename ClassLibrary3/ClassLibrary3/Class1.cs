5c0f69dec39ef61c78fab1a2049bb436be63105d
prisada <prisada28@gmail.com>
2020-10-19 09:59:26 +0300

    <massege>

---
+++ ClassLibrary3/ClassLibrary3/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ClassLibrary2
+{
+    public class Radical
+    {
+        private string a;
+
+        public Radical(string a)
+        {
+            this.a = a;
+        }
+        public string getRad()
+        {
+            return "" + (Math.Sqrt(Int32.Parse(a)));
+        }
+    }
+}
